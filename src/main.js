823d6e22d1bf386f6c00ebf97659c03b6111e988
CharlesBdt <bidaut.charles@gmail.com>
2021-06-20 19:29:53 +0200

    Add Vue-Fontawesome plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router/router';
 import store from '@/store';
+import '@/assets/scss/global.scss';
+import '@/assets/scss/spacing.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSkiing } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faSkiing);
 
 createApp(App)
   .use(store)
   .use(router)
+  .component('fa', FontAwesomeIcon)
   .mount('#app');
